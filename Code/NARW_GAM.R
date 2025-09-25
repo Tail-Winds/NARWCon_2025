85450afe93a8efe0c7cfb829673052eddffb7439
Slava <vlyubchich@users.noreply.github.com>
2025-09-25 01:07:15 -0400

    working version in GAMLSS

--- Code/NARW_GAM.R
+++ Code/NARW_GAM.R
@@ -1,84 +1,166 @@
 rm(list = ls())
-#library(tidyverse)
+
+# Data loading and modification
+# library(tidyverse)
+library(dplyr)
+# library(scales)
+
+# Visualization
 library(ggplot2)
 theme_set(theme_light())
-library(dplyr)
-library(scales)
-library(mgcv)
+library(patchwork)
+library(GGally)
+
+# Modeling
 library(gamlss)
-library(gamlss.tr)
+# library(gamlss.tr)
+library(gamlss.add)
+# library(mgcv)
 
 
-RWocc <- read.csv("DataRaw/WhaleOccurrence2014_2024_091825.csv")
-RWocc<- RWocc %>%
-    mutate(
-        #Year = ifelse(Month %in% month.name[1:10], Year),  #Year + 1
-        Date = as.Date(paste(Year, Month, "01", sep = "-"), format = "%Y-%b-%d")
-    ) %>%
-    mutate(Month = as.numeric(format(Date, "%m")))
-           #Period = ifelse(Year <= 2017, "PAM array", "RTWB")) %>%
-    #select(-Study) %>%
-    #arrange(Species, Date)
-table(RWocc$Year, RWocc$Site)
-table(RWocc$DeviceType, RWocc$Period)
+# Load data ----
+
+RWocc0 <- read.csv("DataRaw/WhaleOccurrence2014_2024_091825.csv")
+RWocc <- RWocc0 %>%
+    mutate(Date = as.Date(paste(Year, Month, "01", sep = "-"), format = "%Y-%b-%d")) %>%
+    mutate(Month = as.numeric(format(Date, "%m"))) %>%
+    # Change Site, Month, and Period (1 = 2014-17, 2 = 2021-2024) to categorical
+    mutate(MonthCat = as.factor(Month),
+           Period = as.factor(Period),
+           Site = as.factor(Site)) %>%
+    # Create lagged values of PercentOccurrence, by Site
+    # group_by(Site) %>%
+    # mutate(PercentOccurrence_l1 = dplyr::lag(PercentOccurrence, 1, order_by = Date),
+    #        PercentOccurrence_l2 = dplyr::lag(PercentOccurrence, 2, order_by = Date)) %>%
+    # ungroup() %>%
+    # Sort by Site and Date
+    arrange(Site, Date)
 
 # Change year to categorical
 RWocc$YearCat <- as.factor(RWocc$Year)
 RWocc$YearCat <- relevel(RWocc$YearCat, "2015") #relevel for intercept to be 2015 not 2014
 
-RWocc$MonthCat <- as.factor(RWocc$Month)
-#RWocc$MonthCat <- relevel(RWocc$MonthCat, "January") #relevel for intercept to be 2015 not 2014
 
-#Change period (1 = 2014-17, 2 = 2021-2024) to categorical
-RWocc$Period <- as.factor(RWocc$Period)
+# Exploratory analysis ----
 
+# Sample sizes for different combinations
+table(RWocc$Year, RWocc$Site)
+table(RWocc$DeviceType, RWocc$Period)
 
-GGally::ggpairs(RWocc %>% dplyr::relocate(PercentOccurrence, .after = last_col())) #creates pairs plot, matrix pairwise relationships of all data
-hist(RWocc$PercentOccurrence) # zero inflated distribution, effect which families might be good options
+# Pairs plot, matrix pairwise relationships of all data
+GGally::ggpairs(RWocc %>% dplyr::relocate(PercentOccurrence, .after = last_col()))
+
+# Histogram of response variable
+hist(RWocc$PercentOccurrence)
+# zero inflated distribution, effect which families might be good options
 mean(RWocc$PercentOccurrence == 0) # proportion of 0s
 sort(RWocc$PercentOccurrence)
 
-boxplot(PercentOccurrence ~ Site, data = RWocc)
+# Boxplots of PercentOccurrence by different categorical variables
+p1 <- ggplot(RWocc, aes(x = as.factor(Year), y = PercentOccurrence)) +
+    geom_boxplot(fill = "lightblue") +
+    xlab("Year")
+p2 <- ggplot(RWocc, aes(x = MonthCat, y = PercentOccurrence)) +
+    geom_boxplot(fill = "lightblue")
+p3 <- ggplot(RWocc, aes(x = Period, y = PercentOccurrence)) +
+    geom_boxplot(fill = "lightblue")
+p4 <- ggplot(RWocc, aes(x = DeviceType, y = PercentOccurrence)) +
+    geom_boxplot(fill = "lightblue")
+p5 <- ggplot(RWocc, aes(x = Site, y = PercentOccurrence)) +
+    geom_boxplot(fill = "lightblue")
+# Combine with patchwork
+(p1 | p2) / (p3 | p4 | p5) +
+    plot_annotation(title = "Percent occurrence by different categorical variables")
 
-interaction.plot( RWocc$YearCat, RWocc$DeviceType, RWocc$PercentOccurrence)
+# Interaction plot
+with(RWocc,
+     interaction.plot(YearCat, DeviceType, PercentOccurrence)
+)
+with(RWocc,
+     interaction.plot(YearCat, MonthCat, PercentOccurrence, col = MonthCat)
+)
 
 
-m_RWocc <- gamlss(PercentOccurrence ~ #pbc(Month, max.df = 5) +
-                   # pb(Year, max.df = 5) +
-                   scs(Month, control = cs.control(cv = FALSE)) +
-                   #ga(~ s(Month, bs = "cc")) +
-                   YearCat +
-                    DeviceType +
-                      scs(Month, control = cs.control(cv = FALSE))*Site
+# Modeling ----
+set.seed(123)
+m_RWocc0 <- gamlss(PercentOccurrence ~ #pbc(Month, max.df = 5) +
+                       # pb(Year, max.df = 5) +
+                       scs(Month, control = cs.control(cv = FALSE)) +
+                       #ga(~ s(Month, bs = "cc")) +
+                       YearCat +
+                       DeviceType +
+                       scs(Month, control = cs.control(cv = FALSE))*Site
                    #MonthCat*YearCat
                    #Period +
                    #DeviceType*YearCat
-               # + Period
-                #+ Year:Period,
-               # random = ~1 | Dummy
-                + PercentOccurrence_l1 + PercentOccurrence_l2, #correlation = corARMA(p = 1),
-               # ), # creates an autocorrelation structure
-               family = ZINBI,
-               control = gamlss.control(c.crit = 0.01, n.cyc = 100),
-               data = RWocc)
-par(mfrow = c(1, 2))
+                   # + Period
+                   #+ Year:Period,
+                   # random = ~1 | Dummy
+                   # + PercentOccurrence_l1 + PercentOccurrence_l2, #correlation = corARMA(p = 1),
+                   # ), # creates an autocorrelation structure
+                   ,family = ZINBI,
+                   control = gamlss.control(c.crit = 0.01, n.cyc = 100),
+                   data = RWocc)
+summary(m_RWocc0)
+
+set.seed(123)
+m_RWocc <- gamlss(PercentOccurrence / 100 ~
+                      ga(~ti(Year, Month)) +
+                      scs(Month, df = 7, control = cs.control(cv = FALSE)) +
+                      # ga(~s(Month, bs = "cp")) +
+                      pb(Year) +
+                      # YearCat + # seems like year as continuous is better
+                      DeviceType
+                  # pvc(Month, by = Site)
+                  ,sigma.formula = ~pb(Month) #+ pb(Year)
+                  ,family = BEINF, #ZINBI,
+                  control = gamlss.control(c.crit = 0.01, n.cyc = 100),
+                  data = RWocc)
+
+## Model diagnostics ----
 plot(m_RWocc)
 plot(m_RWocc, ts = TRUE)
+wp(m_RWocc)
 
+## Term plots ----
+# list all terms
+modterms <- attr(terms(m_RWocc), "term.labels")
 
+png("Figures/RWocc_GAMLSS_TermPlots.png", width = 9, height = 7, units = "in", res = 300)
+par(mfrow = c(ceiling(length(modterms)/2), 2))
+plot(getSmo(m_RWocc, what = "mu", which = 1), scheme = 2, las = 1)
+for (i in 2:length(modterms)) {
+    term.plot(m_RWocc, what = "mu", terms = modterms[i], las = 1)
+}
+dev.off()
+
+# Reset plotting layout
+par(mfrow = c(1, 1))
+# If needed, the 2D plot in a different way
+term.plot(m_RWocc, what = "mu", terms = "ga(~ti(Year, Month))",
+          surface.gam = FALSE)
+
+# Terms for sigma
+term.plot(m_RWocc, what = "sigma")
+
+# Model summary ----
+# although the ga() term doesn't show up in the summary, it is significant
 summary(m_RWocc)
-term.plot(m_RWocc)
 
-## Manual autocorrelation factor (corAR1) method to get around gamlss
-RWocc$PercentOccurrence_l1 = tapply(RWocc$PercentOccurrence, RWocc$Site, function(x) dplyr::lag(x,1)) %>% unlist()
-# created a lagged version of PO variable, instead of adding correlation through corAR1 can add through this lagged variable
-
-#totaldailydph2$DPH_l1 = tapply(totaldailydph2$DPH, totaldailydph2$pod, function(x) c(NA,x[-length(x)])) %>% unlist()
-# can also do corARMA = (p=2)
-
-RWocc$PercentOccurrence_l2 = tapply(RWocc$PercentOccurrence, RWocc$Site, function(x) dplyr::lag(x, 2)) %>% unlist()
-# created a lagged version of PO variable, instead of adding correlation through corAR1 can add through this lagged variable
-
+# Comment out the ga() term to see its effect
+m_RWocc_noYM <- gamlss(PercentOccurrence / 100 ~
+                           # ga(~ti(Year, Month)) +
+                           scs(Month, df = 7, control = cs.control(cv = FALSE)) +
+                           # ga(~s(Month, bs = "cp")) +
+                           pb(Year) +
+                           # YearCat + # seems like year as continuous is better
+                           DeviceType
+                       # pvc(Month, by = Site)
+                       ,sigma.formula = ~pb(Month) #+ pb(Year)
+                       ,family = BEINF, #ZINBI,
+                       control = gamlss.control(c.crit = 0.01, n.cyc = 100),
+                       data = RWocc)
+LR.test(m_RWocc_noYM, m_RWocc)
 
 
 ## Model testing results -- 9/22/25
@@ -96,33 +178,33 @@ RWocc$PercentOccurrence_l2 = tapply(RWocc$PercentOccurrence, RWocc$Site, functio
 
 # Generate new "no year" and "no month" models for RW Likelihood Ratio tests
 m_RWocc_noYear <- gamlss(PercentOccurrence ~ #pbc(Month, max.df = 5) +
-                         # pb(Year, max.df = 5) +
-                         scs(Month, control = cs.control(cv = FALSE))
-                     #+
-                     #ga(~ s(Month, bs = "cc")) +
-                     #YearCat
-                     # + Period
-                     , #+ Year:Period,
-                     # random = ~1 | Dummy
-                     #correlation = corARMA(p = 1)
-                     # ), # creates an autocorrelation structure
-                     family = ZINBI,
-                     control = gamlss.control(c.crit = 0.01, n.cyc = 100),
-                     data = RWocc)
+                             # pb(Year, max.df = 5) +
+                             scs(Month, control = cs.control(cv = FALSE))
+                         #+
+                         #ga(~ s(Month, bs = "cc")) +
+                         #YearCat
+                         # + Period
+                         , #+ Year:Period,
+                         # random = ~1 | Dummy
+                         #correlation = corARMA(p = 1)
+                         # ), # creates an autocorrelation structure
+                         family = ZINBI,
+                         control = gamlss.control(c.crit = 0.01, n.cyc = 100),
+                         data = RWocc)
 
 m_RWocc_noMonth <- gamlss(PercentOccurrence ~ #pbc(Month, max.df = 5) +
-                          # pb(Year, max.df = 5) +
-                          #scs(Month, control = cs.control(cv = FALSE))
-                          #ga(~ s(Month, bs = "cc")) +
-                          YearCat
-                      # + Period
-                      , #+ Year:Period,
-                      # random = ~1 | Dummy
-                      #correlation = corARMA(p = 1)
-                      # ), # creates an autocorrelation structure
-                      family = ZINBI,
-                      control = gamlss.control(c.crit = 0.01, n.cyc = 100),
-                      data = RWocc)
+                              # pb(Year, max.df = 5) +
+                              #scs(Month, control = cs.control(cv = FALSE))
+                              #ga(~ s(Month, bs = "cc")) +
+                              YearCat
+                          # + Period
+                          , #+ Year:Period,
+                          # random = ~1 | Dummy
+                          #correlation = corARMA(p = 1)
+                          # ), # creates an autocorrelation structure
+                          family = ZINBI,
+                          control = gamlss.control(c.crit = 0.01, n.cyc = 100),
+                          data = RWocc)
 
 #Running Likelihood ratio tests for each explanatory variable
 LR.test(m_RWocc_noYear, m_RWocc)
